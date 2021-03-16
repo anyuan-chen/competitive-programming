fca8a3e93493ea01bdc1b4500bb69a127dd463ee
development <andrew.chen.anyuan@gmail.com>
2021-03-16 14:09:37 -0400

    195a

---
+++ AtCoder/Beginner/195a.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld; 
+typedef pair<int, int> pii;
+typedef pair<ll,ll> pll;
+
+int main(){
+    int m, h; cin >> m >> h;
+    if (h%m == 0){
+        cout << "Yes" << endl;
+    }
+    else{
+        cout << "No" << endl;
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
