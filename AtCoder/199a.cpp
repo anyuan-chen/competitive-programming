1d85e9c6c2b961d524e85e0d8e91300976f1b826
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-04-23 19:18:01 -0400

    sync commit

---
+++ AtCoder/199a.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+//this is a comment
+int main()
+{
+}
--- AtCoder/Beginner/196a.cpp
+++ AtCoder/Beginner/196a.cpp
@@ -4,7 +4,7 @@ typedef long long ll;
 typedef long double ld;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
-
+//this is a comment
 int main()
 {
     int a, b, c, d;
@@ -12,3 +12,4 @@ int main()
     cout << max(a, b) - min(c, d);
     return 0;
 }
+
